53584b561d89cbbe3ed53e84fc17a3d7f9d5e81a
Olivier König <34602676+Camelket@users.noreply.github.com>
2024-11-27 11:10:56 +0100

    adjust program.cs

---
+++ Backend/L-Bank.Api/JwtSettings.cs
@@ -0,0 +1,8 @@
+namespace L_Bank_W_Backend;
+
+public class JwtSettings
+{
+    public string? Issuer { get; set; }
+    public string? Audience { get; set; }
+    public string? PrivateKey { get; set; }
+}
