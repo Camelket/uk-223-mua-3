7df64d82f419fa75cf2a7796a2efa366f6e7e1a5
Nico Heinimann <nico.heinimann@bossinfo.ch>
2024-12-03 10:26:18 +0100

    frontend-service

--- Frontend/src/services/booking.service.ts
+++ Frontend/src/services/booking.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable, take } from "rxjs";
+import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 import { Booking, BookingRequest } from "../models/model";
 import { environment } from "../environments/environment";
